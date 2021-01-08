2edf70074699d2b066d1a3b9867982aa595daf66
Ryan Cohen <rcohenprogramming@gmail.com>
2021-01-08 14:22:55 -0500

    Renamed subcommand_functions module into subcommands to shorten the length.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod config;
 mod error;
 mod objects;
-mod subcommand_functions;
+mod subcommands;
 mod utils;
 
 pub use crate::error::{to_git_result, GitError, GitResult};
 pub use crate::objects::{GitBlob, GitFileMode, GitObject, GitRepo};
-pub use crate::subcommand_functions::{hash_object, init};
+pub use crate::subcommands::{hash_object, init};
