8ebe44dc4165b7d3e19639cf32a4ca22f62c6980
Ryan Cohen <rcohenprogramming@gmail.com>
2021-01-08 11:40:49 -0500

    Added GitFileMode to the public API.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,4 +6,4 @@ pub mod subcommand_functions;
 
 pub use error::{to_git_result, GitError, GitResult};
 pub use fileio::{create_dir_all_if_new, create_dir_if_new, read_file, write_if_new};
-pub use objects::{GitBlob, GitObject, GitRepo};
+pub use objects::{GitBlob, GitFileMode, GitObject, GitRepo};
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod config;
 mod core;
 mod subcommands;
 
-pub use crate::core::{GitBlob, GitError, GitObject, GitRepo, GitResult};
+pub use crate::core::{GitBlob, GitError, GitFileMode, GitObject, GitRepo, GitResult};
 pub use crate::subcommands::hash_object::run as hash_object;
 pub use crate::subcommands::init;
